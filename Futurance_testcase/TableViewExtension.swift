8deb56ac4894b0718ca783aeb6f1d5879f8e2afc
Okan Aslan <okannaslann@hotmail.com>
2022-09-22 09:15:40 +0300

    bakiye labeli sondan bir önceki haline getirildi

---
+++ Futurance_testcase/TableViewExtension.swift
@@ -0,0 +1,61 @@
+//
+//  TableViewExtension.swift
+//  Futurance_testcase
+//
+//  Created by MacBook on 22.09.2022.
+//
+
+import Foundation
+import UIKit
+
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let myStr = Double(textField.text!) {
+            let myquantity = myStr
+            //print(myquantity * currentCoinPrice)
+            total = String(myquantity * currentCoinPrice)
+            totalTextField.text = total
+        }
+        closeKeyboard()
+        return true
+    }
+    
+    //cell'e basınca çalışıyor
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let myData = currencyData[indexPath.row]
+        highLabel.text = myData.highPrice
+        lowLabel.text = myData.lowPrice
+        if let cost = Double(myData.price) {
+            currentCoinPrice = cost
+            //print(currentCoinPrice)
+        }
+        currentCoinName = myData.soloCoinName
+        if alEtkili == true {
+            coinAlButton.setTitle("\(currentCoinName) AL", for: UIControl.State.normal)
+        } else {
+            coinAlButton.setTitle("\(currentCoinName) SAT", for: UIControl.State.normal)
+        }
+        //
+        if let coinquantity = Double(coinMiktarTextField.text!) {
+            total = String(coinquantity * currentCoinPrice)
+            totalTextField.text = total
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! TableViewCell
+        cell.setup(with: currencyData[indexPath.row])
+        cell.cellView.layer.cornerRadius = 20
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currencyData.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+}
--- Futurance_testcase/ViewController.swift
+++ Futurance_testcase/ViewController.swift
@@ -7,7 +7,8 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+
+class ViewController: UIViewController, UITextFieldDelegate {
     
     var newbakiyeString : String = ""
     @IBOutlet weak var bakiyeLabel: UILabel!
@@ -22,8 +23,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     var greenColor = UIColor(red: 77/255.0, green: 199/255.0, blue: 152/255.0, alpha: 1)
     var redColor = UIColor(red: 241/255.0, green: 105/255.0, blue: 99/255.0, alpha: 1)
-    
-
     @IBOutlet weak var totalTextField: UITextField!
     @IBOutlet weak var coinMiktarTextField: UITextField!
     @IBOutlet weak var lowLabel: UILabel!
@@ -34,8 +33,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
-        //view.addGestureRecognizer(gestureRecognizer)
         girisAyarları()
         tableView.delegate = self
         tableView.dataSource = self
@@ -43,37 +40,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         tableView.separatorStyle = .none
         tableView.showsVerticalScrollIndicator = false
-        // Do any additional setup after loading the view.
         window.keyboardSlide()
         getData()
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
-    }
-    
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if let myStr = Double(textField.text!) {
-            let myquantity = myStr
-            //print(myquantity * currentCoinPrice)
-            total = String(myquantity * currentCoinPrice)
-            totalTextField.text = total
-        }
-        closeKeyboard()
-        return true
-    }
-    
-    /*
-    @IBAction func actionTriggered(_ sender: UITextField) {
-        if let myStr = Double(textField.text!) {
-            let myquantity = myStr
-            print(myquantity * currentCoinPrice)
-            let total = String(myquantity * currentCoinPrice)
-            totalTextField.text = total
         
     }
-     */
     
     @objc func closeKeyboard() {
         view.endEditing(true)
@@ -94,35 +64,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         ApiService().loadTickerPrice(baseSymbol: appSetting.baseSymbol) { result in
             if result != nil {
                 DispatchQueue.main.async {
-//                    print("get datadaki result : \(result?[0].name)")
-                    //print(result as Any)
                     self.tableView.reloadData()
                 }
             }
         }
     }
     
-    /*
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        if let myStr = Double(textField.text!) {
-            let myquantity = myStr
-            //print(myquantity * currentCoinPrice)
-            let total = String(myquantity * currentCoinPrice)
-            totalTextField.text = total
-        }
-    }
-*/
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return currencyData.count
-    }
-    
-    
     @IBAction func editChange(_ sender: UITextField){
         
         if sender.text == "" {
             totalTextField.text = "0"
-        }        
+        }
         if let myStr = Double(sender.text!) {
             let myquantity = myStr
             //print(myquantity * currentCoinPrice)
@@ -130,39 +82,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             totalTextField.text = total
         }
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! TableViewCell
-        cell.setup(with: currencyData[indexPath.row])
-        cell.cellView.layer.cornerRadius = 20
-        
-        return cell
-    }
-    
-    
-    //cell'e basınca çalışıyor
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let myData = currencyData[indexPath.row]
-        highLabel.text = myData.highPrice
-        lowLabel.text = myData.lowPrice
-        if let cost = Double(myData.price) {
-            currentCoinPrice = cost
-            //print(currentCoinPrice)
-        }
-        currentCoinName = myData.soloCoinName
-        if alEtkili == true {
-            coinAlButton.setTitle("\(currentCoinName) AL", for: UIControl.State.normal)
-        } else {
-            coinAlButton.setTitle("\(currentCoinName) SAT", for: UIControl.State.normal)
-        }
-        //
-        if let coinquantity = Double(coinMiktarTextField.text!) {
-            total = String(coinquantity * currentCoinPrice)
-            totalTextField.text = total
-        }
-        
-    }
-    
     
     @IBAction func alButtonTapped(_ sender: UIButton) {
         coinAlButton.backgroundColor = greenColor
@@ -179,9 +98,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         alEtkili = false
         coinAlButton.setTitle("\(currentCoinName) SAT", for: UIControl.State.normal)
     }
-    
-    
-    
+        
     @IBAction func coinAlButtonTapped(_ sender: UIButton) {
         if alEtkili == true {
             if let alinanCoinTutari = Double(totalTextField.text!) {
@@ -192,25 +109,41 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 else {
                     if currentCoinName == "" {
                         uyariVer(mesaj: "Coin Seçmediniz")
+                    
                     }else {
+                        /*
+                        for (name) in myWallet.keys {
+                            if currentCoinName == name {
+                                myWallet[name]! += alinanCoinTutari
+                                myWallet["TRY"] = myWallet["TRY"]! - alinanCoinTutari
+                            }else {
+                                myWallet[currentCoinName] = alinanCoinTutari
+                                myWallet["TRY"] = myWallet["TRY"]! - alinanCoinTutari
+                            }
+                        }
+                         
+                         */
                         print("alıyoruz")
                         //print(alinanCoinTutari)
                         myWallet[currentCoinName] = alinanCoinTutari
                         myWallet["TRY"] = myWallet["TRY"]! - alinanCoinTutari
-                        bakiyeLabel.text = "\(coinMiktarTextField.text!) \(currentCoinName), \(alinanCoinTutari) TRY  "
+                        // ALTTAKİ SATIR EĞER SON İŞLEM BAKİYE OLARAK GÖZÜKECEKSE AKTİF HALE GELECEK
+                        //bakiyeLabel.text = "\(coinMiktarTextField.text!) \(currentCoinName), \(alinanCoinTutari) TRY  "
                     }
                 }
             }
-            //print(myWallet["TRY"]!)
-            /*
+            // Aşağıdaki Satırlar eğer ki cüzdan toplam bakiyesi istenirse aktif hale gelecek
+            
+            bakiyeString = ""
             for (coin, price) in myWallet {
                 newbakiyeString = "\(price) \(coin) "
                 bakiyeString.append(contentsOf: newbakiyeString)
             }
             bakiyeLabel.text = bakiyeString
-            print(bakiyeString)
+            print("Bakiye Stringi : \(bakiyeString)")
             print("*****")
-             */
+            
+            
         }
         else if alEtkili == false {
             if let alinanCoinTutari = Double(totalTextField.text!) {
@@ -240,3 +173,4 @@ extension String {
         return NumberFormatter().number(from: self)?.doubleValue
     }
 }
+
